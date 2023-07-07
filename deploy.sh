6d25b3957290b134148fe157551f5251b3df5674
Ajaydurga17 <83573329+Ajaydurga17@users.noreply.github.com>
2023-07-07 20:23:25 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 result=$( sudo docker images -q my-app:latest )
-docker run -itd --name pro-container -p 3001:80 $result
+docker run -itd --name pro-container -p 80:80 $result
 CONTAINER_NAME='pro-container'
 x=$(docker ps -q -f status=running -f name=$pro-container)
 if [[ -n "$x" ]]; then
