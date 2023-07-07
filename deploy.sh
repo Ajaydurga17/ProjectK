bcb4e10beb89cd5d84f6ed7a1e215689028bba0a
Ubuntu <ubuntu@ip-172-31-84-102.ec2.internal>
2023-07-07 10:32:25 +0000

    comming all files to dev

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+result=$( sudo docker images -q my-app:latest )
+docker run -itd --name pro-container -p 3000:80 $result
+CONTAINER_NAME='pro-container'
+x=$(docker ps -q -f status=running -f name=$pro-container)
+if [[ -n "$x" ]]; then
+  echo 'Container exists'
+else
+  echo 'No container image'
+fi
